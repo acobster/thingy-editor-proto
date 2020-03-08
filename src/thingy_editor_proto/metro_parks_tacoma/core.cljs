7b06f263f349e2b894764eea89224e3dca527611
Coby Tamayo <acobster@gmail.com>
2020-03-08 14:21:11 -0700

    format for clarity

--- src/thingy_editor_proto/metro_parks_tacoma/core.cljs
+++ src/thingy_editor_proto/metro_parks_tacoma/core.cljs
@@ -5,10 +5,14 @@
      [thingy.backend.http :as http-backend]))
 
 
-(def media-library [{:uri "/metroparkstacoma/media-library/fort-hero-2-345x390.jpg" :alt "Fort Hero"}
-                    {:uri "/metroparkstacoma/media-library/Marina_8LaneBoatLaunch_FacingBreakwater-345x390.jpg" :alt "Boat Launcher"}
-                    {:uri "/metroparkstacoma/media-library/hero-baja-bay-girl-boy-shark-1440x500-crop-1578494576-861x500.jpg" :alt "Baja Bay"}
-                    {:uri "/metroparkstacoma/media-library/nwtrek-715x415.jpg" :alt "NW Trek"}])
+(def media-library [{:uri "/metroparkstacoma/media-library/fort-hero-2-345x390.jpg"
+                     :alt "Fort Hero"}
+                    {:uri "/metroparkstacoma/media-library/Marina_8LaneBoatLaunch_FacingBreakwater-345x390.jpg"
+                     :alt "Boat Launcher"}
+                    {:uri "/metroparkstacoma/media-library/hero-baja-bay-girl-boy-shark-1440x500-crop-1578494576-861x500.jpg"
+                     :alt "Baja Bay"}
+                    {:uri "/metroparkstacoma/media-library/nwtrek-715x415.jpg"
+                     :alt "NW Trek"}])
 
 
 
