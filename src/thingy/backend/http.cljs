4049ce08cb7d198b083f16147044d3269926468d
Coby Tamayo <acobster@gmail.com>
2020-03-08 11:14:13 -0700

    refactor http backend to use cljs-http

--- src/thingy/backend/http.cljs
+++ src/thingy/backend/http.cljs
@@ -4,27 +4,26 @@
    [thingy.dom :as dom]))
 
 
-(defn conf->endpoint [{:keys [endpoint] :as http-conf} event elem]
+(defn ->endpoint [event elem {:keys [endpoint] :as http-conf}]
   (cond
     (string? endpoint) endpoint
     (fn? endpoint) (endpoint event elem)
     (nil? http-conf) (js/console.warn "No HTTP backend config detected")
     :else (js/console.warn "No endpoint configured" (clj->js http-conf))))
 
-(defn event->request [event elem conf]
-  (let [ident (.-thingyIdent (dom/dataset elem))]
-    {:method "POST"
-     :headers {"Content-Type" "application/json"}
-     :body (.stringify js/JSON (clj->js {:ident ident
-                                         :content (.-innerHTML elem)}))}))
+(defn event->request [event elem _]
+  {:json-params {:ident (.-thingyIdent (dom/dataset elem))
+                 :attrs (:attrs event)
+                 :content (.-innerHTML elem)}})
 
 
 (defn ^:export update! [event elem conf]
-  (let [req (event->request event elem conf)
-        promise (js/fetch (conf->endpoint (:http conf) event elem) (clj->js req))]
-    (.then promise
-           (fn [response]
-             (js/console.log response)))))
+  (let [http-conf (:http conf)
+        ->req (or (:event->request http-conf) event->request)
+        method (or (:method http-conf) http/post)
+        endpoint (->endpoint event elem http-conf)
+        req (->req event elem conf)]
+    (method endpoint req)))
 
 
 (defonce ^:export backend {:name :dom
